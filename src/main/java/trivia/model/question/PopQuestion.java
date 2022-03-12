a82693c54e849fc3ba28f2e1cbb63b8d19c053da
Pavle Prica <pavle.prica5@gmail.com>
2022-03-12 18:36:13 +0100

    Add Question and implementations
    This is done to avoid huge Switches. We should rather focus on different
    implementations.

---
+++ src/main/java/trivia/model/question/PopQuestion.java
@@ -0,0 +1,8 @@
+package trivia.model.question;
+
+public class PopQuestion extends Question {
+
+    public PopQuestion(int index) {
+        super("Pop question " + index);
+    }
+}
---
+++ src/main/java/trivia/model/question/RockQuestion.java
@@ -0,0 +1,8 @@
+package trivia.model.question;
+
+public class RockQuestion extends Question {
+
+    public RockQuestion(int index) {
+        super("Rock question " + index);
+    }
+}

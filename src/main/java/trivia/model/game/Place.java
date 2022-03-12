d98dfce3e040955e521a44b51fadb76123b91fe9
Pavle Prica <pavle.prica5@gmail.com>
2022-03-12 19:30:29 +0100

    Refactor Place to use Deque

--- src/main/java/trivia/model/game/Place.java
+++ src/main/java/trivia/model/game/Place.java
@@ -1,21 +1,21 @@
 package trivia.model.game;
 
-import java.util.LinkedList;
+import java.util.Deque;
 import trivia.model.question.Question;
 
 public class Place {
 
     private final int placeLocation;
 
-    private LinkedList<Question> questionStack;
+    private Deque<Question> questionStack;
 
-    public Place(int placeLocation, LinkedList<Question> questionStack) {
+    public Place(int placeLocation, Deque<Question> questionStack) {
         this.placeLocation = placeLocation;
         this.questionStack = questionStack;
     }
 
     private Question drawQuestionFromStack() {
-        return questionStack.removeFirst();
+        return questionStack.pop();
     }
 
     public int getPlaceLocation() {
